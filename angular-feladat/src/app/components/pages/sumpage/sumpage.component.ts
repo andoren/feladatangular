547fbf4dd68920878c10c5c208eaf2dfcb5dd9c8
andoren <mpekar55@gmail.com>
2020-08-04 20:29:03 +0200

    sumpagehez is hozzáadva a toast és a loading spinner

--- angular-feladat/src/app/components/pages/sumpage/sumpage.component.ts
+++ angular-feladat/src/app/components/pages/sumpage/sumpage.component.ts
@@ -3,6 +3,7 @@ import { Product } from 'src/app/models/Product';
 import { ActivatedRoute } from '@angular/router';
 import { Productservice } from 'src/app/service/productservice.service';
 import { Shared } from 'src/app/models/Shared';
+import { ToastService } from 'src/app/service/toast.service';
 
 @Component({
   selector: 'app-sumpage',
@@ -12,13 +13,27 @@ import { Shared } from 'src/app/models/Shared';
 export class SumpageComponent implements OnInit {
   id:number;
   product:Product;
-  constructor(private route:ActivatedRoute, private productService:Productservice,private  shared:Shared) { }
+  _isLoading:boolean;
+  constructor(private route:ActivatedRoute, private productService:Productservice,private  toastService:ToastService) { }
 
   ngOnInit(): void {
+    this.setIsLoading(true);
     this.id = Number(this.route.snapshot.paramMap.get('id'));
     this.productService.getProductById(this.id).subscribe(product=>{{
-      this.product = product;      
-    }});
+      this.product = product;
+            
+    }},(error)=>{
+      this.toastService.showError("Hiba az oldal letöltése közben. Sajnáljuk ! :(","Hiba a letöltés közben.");
+    },()=>{
+      this.setIsLoading(false);
+    });
     
+  }
+  setIsLoading(bool:boolean):void{
+    this._isLoading = bool;
+  }
+  getIsLoading():boolean{
+
+    return this._isLoading;
   }
 }
